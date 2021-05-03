74ebb704f6af537c2ab881d228d850acb76f1737
Zdravko Zdravkov <zdr0@yahoo.com>
2021-05-03 14:40:50 +0200

    Playing with Unity 3d

---
+++ 01. UNITY BASICS/Lesson1/Assets/Scripts/FollowScript.cs
@@ -0,0 +1,27 @@
+﻿// ReSharper disable ArrangeTypeMemberModifiers
+// ReSharper disable CheckNamespace
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnassignedField.Global
+
+using UnityEngine;
+
+public class FollowScript : MonoBehaviour
+{
+    public Transform sphere;
+    public float followSpeed;
+    public float minDistance;
+
+    // Update is called once per frame
+	void LateUpdate ()
+    {
+        if (this.sphere != null)
+        {
+            this.transform.LookAt(this.sphere);
+
+            if (Vector3.Distance(this.transform.position, this.sphere.position) > this.minDistance)
+            {
+                this.transform.Translate(0F, 0F, this.followSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
